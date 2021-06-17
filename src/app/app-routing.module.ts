5391b51235015d22347b21bddfc0899e94bfb617
katerina-tziala <katerina.tziala@gmail.com>
2021-06-17 20:16:10 +0200

    try routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CityForecastComponent } from './city-forecast/city-forecast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'city', component: CityForecastComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CityForecastComponent } from './city-forecast/city-forecast.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      CityForecastComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
