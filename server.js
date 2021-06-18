f9f1968487b910942e1088caffd39f1188ece3ca
katerina-tziala <katerina.tziala@gmail.com>
2021-06-18 23:38:38 +0200

    update for build

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('./dist/app'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('./dist/app/index.html');
+  res.sendFile('/dist/app/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
